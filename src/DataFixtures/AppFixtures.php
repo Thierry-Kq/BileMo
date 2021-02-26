1f7d141f65a6607678a5e8c328b880c30a0a1866
K-pc-Ubuntu <kasskq@gmail.com>
2021-02-26 22:16:33 +0100

    Add a client (User) via Security & jwt instal & initial setup

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,9 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Client;
 use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
@@ -16,10 +18,29 @@ class AppFixtures extends Fixture
         'Xmeria 12',
     ];
 
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
 
     public function load(ObjectManager $manager)
     {
 
+        // 10 client
+        for ($i = 0; $i < 10; $i++) {
+
+            $client = new Client();
+
+            $client->setLogin('azerty-' . $i);
+            $client->setPassword(
+                $this->passwordEncoder->encodePassword($client, 'azerty')
+            );
+            $manager->persist($client);
+        }
+
+        // products
         foreach (self::$productsList as $productName) {
 
             $product = new Product();
